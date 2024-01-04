f8551c72b4d5e06307104be9350aaeb520456a5e
Denys Horobzieiev <denis.gorobzeev@gmail.com>
2024-01-04 10:54:01 +0200

    Add readme file

--- ios/RnAnimationsLibraryTests/rn_cleanTests.m
+++ ios/RnAnimationsLibraryTests/rn_cleanTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rn_cleanTests : XCTestCase
+@interface RnAnimationsLibraryTests : XCTestCase
 
 @end
 
-@implementation rn_cleanTests
+@implementation RnAnimationsLibraryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
